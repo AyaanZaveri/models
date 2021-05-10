ea5009e10b6f41802b71cd103fd169d1ed7eabe4
Ayaan Zaveri <63752541+AyaanZaveri@users.noreply.github.com>
2021-05-10 09:39:19 -0400

    Update visualization_utils.py

--- research/object_detection/utils/visualization_utils.py
+++ research/object_detection/utils/visualization_utils.py
@@ -209,7 +209,7 @@ def draw_bounding_box_on_image(image,
               width=thickness,
               fill=color)
   try:
-    font = ImageFont.truetype('ProximaNova-Regular.ttf', 12)
+    font = ImageFont.truetype('Proxima Nova Bold.ttf', 12)
   except IOError:
     font = ImageFont.load_default()
 
